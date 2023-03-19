c511d283e97c8760d15a466732716c946a970844
Mark <marktbuenaflor@gmail.com>
2023-03-19 11:11:05 -0700

    changes to nextjs config for unoptimized images

--- react-chrome-extension/next.config.js
+++ react-chrome-extension/next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains:['mhxmexdtagjbmquxeusj.supabase.co']
+    domains:['mhxmexdtagjbmquxeusj.supabase.co'],
+    unoptimized: true
   }
 }
 
